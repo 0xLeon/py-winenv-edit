698c3acdacf9c5b56fdd580d8cb4203867a37f93
Stefan <Stefan_Hahn1@gmx.net>
2017-12-01 11:26:42 +0100

    Indent using tabs

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='winenv-edit',
-    version='0.0.0',
+	name='winenv-edit',
+	version='0.0.0',
 
-    url='https://github.com/0xLeon/py-winenv-edit',
-    author='Stefan Hahn',
-    author_email='developement@0xleon.com',
+	url='https://github.com/0xLeon/py-winenv-edit',
+	author='Stefan Hahn',
+	author_email='developement@0xleon.com',
 
-    license='MIT',
+	license='MIT',
 
-    packages=find_packages(),
+	packages=find_packages(),
 
-    install_requires=[
-        'winenv',
-        'elevate',
-    ],
+	install_requires=[
+		'winenv',
+		'elevate',
+	],
 )
--- winenvedit/__init__.py
+++ winenvedit/__init__.py
@@ -10,46 +10,46 @@ import winenv
 __version__ = '0.0.0'
 
 def prepend_env_var(name, value, env=winenv.ENV_USER, *other):
-    winenv.append_env_var(name, value, True, ';', env)
+	winenv.append_env_var(name, value, True, ';', env)
 
 def append_env_var(name, value, env=winenv.ENV_USER, *other):
-    winenv.append_env_var(name, value, False, ';', env)
+	winenv.append_env_var(name, value, False, ';', env)
 
 def set_env_var(name, value, env=winenv.ENV_USER, overwrite=True, *other):
-    if value.upper() == 'PATH' and overwrite:
-        raise EnvironmentError('Overwriting the PATH environment variable is not supported. Use the append or prepend action instead.')
+	if value.upper() == 'PATH' and overwrite:
+		raise EnvironmentError('Overwriting the PATH environment variable is not supported. Use the append or prepend action instead.')
 
-    if len(value) == 0:
-        # TODO: implement deletion in winenv
-        pass
-    else:
-        winenv.set_env_var(name, value, overwrite=overwrite, env=env)
+	if len(value) == 0:
+		# TODO: implement deletion in winenv
+		pass
+	else:
+		winenv.set_env_var(name, value, overwrite=overwrite, env=env)
 
 def main():
-    actions = {
-        'set': set_env_var,
-        'prepend': prepend_env_var,
-        'append': append_env_var
-    }
+	actions = {
+		'set': set_env_var,
+		'prepend': prepend_env_var,
+		'append': append_env_var
+	}
 
-    envs = [winenv.ENV_USER, winenv.ENV_SYSTEM]
+	envs = [winenv.ENV_USER, winenv.ENV_SYSTEM]
 
-    p = argparse.ArgumentParser()
-    p.add_argument('action', choices=['set', 'prepend', 'append'], metavar='action')
-    p.add_argument('-m', '--system', action='store_true', dest='system')
-    p.add_argument('-f', '--force', action='store_true', dest='overwrite')
-    p.add_argument('vars', nargs='+', metavar='name=[value]')
+	p = argparse.ArgumentParser()
+	p.add_argument('action', choices=['set', 'prepend', 'append'], metavar='action')
+	p.add_argument('-m', '--system', action='store_true', dest='system')
+	p.add_argument('-f', '--force', action='store_true', dest='overwrite')
+	p.add_argument('vars', nargs='+', metavar='name=[value]')
 
-    args = p.parse_args()
+	args = p.parse_args()
 
-    if not args.system or elevate.elevate():
-        for var in args.vars:
-            if '=' not in var:
-                print('Skipping invalid var {}'.format(var), file=sys.stderr)
-                continue
+	if not args.system or elevate.elevate():
+		for var in args.vars:
+			if '=' not in var:
+				print('Skipping invalid var {}'.format(var), file=sys.stderr)
+				continue
 
-            name, value = var.split('=', 1)
-            actions[args.action](name, value, envs[int(args.system)], args.overwrite)
+			name, value = var.split('=', 1)
+			actions[args.action](name, value, envs[int(args.system)], args.overwrite)
 
 if __name__ == '__main__':
-    main()
+	main()
